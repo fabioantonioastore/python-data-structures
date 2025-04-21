fd3777e08d304a85a09b5d596476a17314859b1a
ninja <astore.a.fabio@gmail.com>
2025-04-21 10:03:49 -0300

    function overhead fixed

--- data_structures/rotation_linked_list.py
+++ data_structures/rotation_linked_list.py
@@ -14,26 +14,26 @@ class RotationLinkedList(LinkedList):
         if self.is_empty() or len(self) == 1:
             return
         if reverse:
-            for _ in range(times):
-                self.__reverse_rotation()
+            self.__reverse_rotation(times)
             return
+        self.__rotation(times)
+
+    def __reverse_rotation(self, times: int) -> None:
         for _ in range(times):
-            self.__rotation()
+            node = self.head_node
+            node.prev = self.tail_node
+            self.tail_node.next = node
+            self.head_node = node.next
+            node.next = None
+            self.head_node.prev = None
+            self.tail_node = node
 
-    def __reverse_rotation(self) -> None:
-        node = self.head_node
-        node.prev = self.tail_node
-        self.tail_node.next = node
-        self.head_node = node.next
-        node.next = None
-        self.head_node.prev = None
-        self.tail_node = node
-
-    def __rotation(self) -> None:
-        node = self.tail_node
-        node.next = self.head_node
-        self.head_node.prev = node
-        self.head_node = node
-        self.tail_node = node.prev
-        self.tail_node.next = None
-        node.prev = None
+    def __rotation(self, times: int) -> None:
+        for _ in range(times):
+            node = self.tail_node
+            node.next = self.head_node
+            self.head_node.prev = node
+            self.head_node = node
+            self.tail_node = node.prev
+            self.tail_node.next = None
+            node.prev = None
--- data_structures/rotation_queue.py
+++ data_structures/rotation_queue.py
@@ -16,29 +16,29 @@ class RotationQueue(Queue):
         if self.is_empty() or len(self) == 1:
             return
         if reverse:
-            for _ in range(times):
-                self.__reverse_rotation()
+            self.__reverse_rotation(times)
             return
+        self.__rotation(times)
+
+    def __reverse_rotation(self, times: int) -> None:
         for _ in range(times):
-            self.__rotation()
+            node = self.first_node
+            node.prev = self.last_node
+            self.last_node.next = node
+            self.first_node = node.next
+            node.next = None
+            self.first_node.prev = None
+            self.last_node = node
 
-    def __reverse_rotation(self) -> None:
-        node = self.first_node
-        node.prev = self.last_node
-        self.last_node.next = node
-        self.first_node = node.next
-        node.next = None
-        self.first_node.prev = None
-        self.last_node = node
-
-    def __rotation(self) -> None:
-        node = self.last_node
-        node.next = self.first_node
-        self.first_node.prev = node
-        self.first_node = node
-        self.last_node = node.prev
-        self.last_node.next = None
-        node.prev = None
+    def __rotation(self, times: int) -> None:
+        for _ in range(times):
+            node = self.last_node
+            node.next = self.first_node
+            self.first_node.prev = node
+            self.first_node = node
+            self.last_node = node.prev
+            self.last_node.next = None
+            node.prev = None
 
     def __repr__(self) -> str:
         return (
